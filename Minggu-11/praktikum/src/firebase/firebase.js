1b6dc8f3d1fdeb53af348a6b52a34d4c7b39ed97
nandadesu <43083923+nandadesu@users.noreply.github.com>
2021-04-26 11:55:34 +0700

    summit

--- Minggu-11/praktikum/src/firebase/firebase.js
+++ Minggu-11/praktikum/src/firebase/firebase.js
@@ -1,4 +1,9 @@
-export default {
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyAM1H9w-mESnmOgSWgv_XEVF9KDCi-03PM",
   authDomain: "praktikum-a0303.firebaseapp.com",
   databaseURL: "https://praktikum-a0303-default-rtdb.firebaseio.com",
@@ -7,4 +12,8 @@ export default {
   messagingSenderId: "1015630968545",
   appId: "1:1015630968545:web:d5372d6a59ea490d8569a0",
   measurementId: "G-DBE2468RVX"
-};
+  };
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;
--- Minggu-11/praktikum/src/index.js
+++ Minggu-11/praktikum/src/index.js
@@ -1,44 +1,11 @@
-import React, {useState} from "react";
-import ReactDOM from "react-dom";
-import {Switch, BrowserRouter as Router, Route} from "react-router-dom";
-import routes from "./routes.js";
-import Header from "./Header.js";
-import "./Style.css";
-import firebase from "firebase";
-import firebaseConfig from "./firebase.config";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from "./root";
+import reportWebVitals from './reportWebVitals';
 
-firebase.initializeApp(firebaseConfig);
+ReactDOM.render(<Root />,document.getElementById('root'));
 
-export const AuthContext = React.createContext(null);
-
-export function App() {
-  const [isLoggedIn, setLoggedIn] = useState(false);
-
-  return (
-    <AuthContext.Provider value={{isLoggedIn, setLoggedIn}}>
-      Is logged in? {JSON.stringify(isLoggedIn)}
-      <div className="App">
-        <Router>
-          <Header />
-
-          <Switch>
-            {routes.map(route => (
-              <Route
-                key={route.path}
-                path={route.path}
-                exact={route.exact}
-                component={route.main}
-              />
-            ))}
-          </Switch>
-        </Router>
-      </div>
-    </AuthContext.Provider>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
